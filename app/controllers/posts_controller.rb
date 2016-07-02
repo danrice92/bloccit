ab98f72c28c8ea05d8d21b344d10fc9860483719
Dan Rice <dan.rice.92@outlook.com>
2016-07-02 04:02:16 +0000

    Assignment 31: Rails Intro to Crud completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.map.with_index { |post, index| post.title.replace "SPAM" if (index == 0 || index % 5 == 0) }
   end
 
   def show
