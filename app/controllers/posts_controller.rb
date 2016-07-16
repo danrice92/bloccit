29a86de1a706d4e801901cbc26d1c94704f1bce9
Dan Rice <dan.rice.92@outlook.com>
2016-07-16 15:57:07 +0000

    Checkpoint 42: Rails Labels completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,7 @@ class PostsController < ApplicationController
     @post = @topic.posts.build(post_params)
     @post.user = current_user
     if @post.save
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was saved successfully."
       redirect_to [@topic, @post]
     else
@@ -34,6 +35,7 @@ class PostsController < ApplicationController
     @post.assign_attributes(post_params)
     
     if @post.save
+      @post.labels = Label.update_labels(params[:post][:labels])
       flash[:notice] = "Post was updated successfully."
       redirect_to [@post.topic, @post]
     else
