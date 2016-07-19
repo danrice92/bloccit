e6612836be21b1ede8cb5e5a20447faffb78f632
Dan Rice <dan.rice.92@outlook.com>
2016-07-19 15:01:55 +0000

    Checkpoint 43: Rails Voting completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160716150825) do
-
-  create_table "advertisements", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20160719144750) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -56,29 +48,22 @@ ActiveRecord::Schema.define(version: 20160716150825) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
+  create_table "topiccomments", force: :cascade do |t|
     t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sponsored_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
-  add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
+  add_index "topiccomments", ["topic_id"], name: "index_topiccomments_on_topic_id"
+  add_index "topiccomments", ["user_id"], name: "index_topiccomments_on_user_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
@@ -96,5 +81,16 @@ ActiveRecord::Schema.define(version: 20160716150825) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
