d71969d2defa5fb826e8f616b93ab6a53bcf4e8b
Dan Rice <dan.rice.92@outlook.com>
2016-07-22 20:34:15 +0000

    Checkpoint 45: Rails Public Profiles completed

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe Post, type: :model do
   let(:description) { RandomData.random_paragraph }
   let(:title) { RandomData.random_sentence }
   let(:body) { RandomData.random_paragraph }
-  let(:topic) { Topic.create!(name: name, description: description) }
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
-  let(:post) { topic.posts.create!(title: title, body: body, user: user) }
+  let(:topic) { create(:topic) }
+  let(:user) { create(:user) }
+  let(:post) { create(:post) }
   
   it { is_expected.to have_many(:labelings) }
   it { is_expected.to have_many(:labels).through(:labelings) }
@@ -27,8 +27,8 @@ RSpec.describe Post, type: :model do
   it { is_expected.to validate_length_of(:body).is_at_least(20) }
   
   describe "attributes" do
-    it "has title, body, and user attributes" do
-      expect(post).to have_attributes(title: title, body: body, user: user)
+    it "has title and body attributes" do
+      expect(post).to have_attributes(title: post.title, body: post.body)
     end
   end
   
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,18 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-    let(:name) { RandomData.random_sentence }
-    let(:description) { RandomData.random_paragraph }
     let(:public) { true }
-    let(:topic) { Topic.create!(name: name, description: description) }
+    let(:topic) { create(:topic) }
     
     it { is_expected.to have_many(:posts) }
     it { is_expected.to have_many(:labelings) }
     it { is_expected.to have_many(:labels).through(:labelings) }
     
     describe "attributes" do
-        it "has name, description, and public attributes" do
-            expect(topic).to have_attributes(name: name, description: description, public: public)
+        it "has name and description attributes" do
+            expect(topic).to have_attributes(name: topic.name, description: topic.description)
         end
         
         it "is public by default" do
