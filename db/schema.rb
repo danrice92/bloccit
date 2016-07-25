8b28ea2641a0542716e1511a5a84329179934931
Dan Rice <dan.rice.92@outlook.com>
2016-07-25 16:15:52 +0000

    Checkpoint 48: Rails API Retrieving Data completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720195349) do
+ActiveRecord::Schema.define(version: 20160725150152) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 20160720195349) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
